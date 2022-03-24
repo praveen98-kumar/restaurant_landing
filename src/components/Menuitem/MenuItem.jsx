a419998ef14f47f6c918a38f35ad8410a6fedd46
Praveen Kumar <pkkoram98@gmail.com>
2022-03-24 12:47:19 +0530

    menu section done

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className='app__menuitem'>
+    <div className="app__menuitem__head">
+      <div className="app__menuitem__name">
+        <p className='p__cormorant' style={{ color: '#dcca87' }}>{title}</p>
+      </div>
+      <div className='app__menuitem__dash' />
+      <div className="app__menuitem__price">
+        <p className='p__cormorant' >{price}</p>
+      </div>
+    </div>
+
+    <div className="app__menuitem__sub">
+      <p className="p__opensans" style={{ color: '#aaa' }}>{tags}</p>
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
-
+import { SubHeading, MenuItem } from '../../components'
+import { images, data } from '../../constants'
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
-  </div>
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className="app__specialMenu__title">
+      <SubHeading title="Menu that fits you palatte" />
+      <h1 className="headtext__cormorant">Today's Special</h1>
+    </div>
+
+    <div className="app__specialMenu__menu">
+      <div className="app__specialMenu__menu__wine flex__center">
+        <p className='app__specialMenu__menu__heading'>Wind & Beer</p>
+        <div className="app__specialMenu__menu__items">
+          {
+            data.wines.map((item, index) => (
+              <MenuItem key={item.title + index} price={item.price} title={item.title} tags={item.tags} />
+            ))
+          }
+        </div>
+      </div>
+
+      <div className='app__specialMenu__menu__img'>
+        <img src={images.menu} alt="menu img" />
+      </div>
+
+      <div className="app__specialMenu__menu__cocktails flex__center">
+        <p className='app__specialMenu__menu__heading'>Cocktails</p>
+        <div className="app__specialMenu__menu__items">
+          {
+            data.cocktails.map((item, index) => (
+              <MenuItem key={item.title + index} title={item.title} price={item.price} tags={item.tags} />
+            ))
+          }
+        </div>
+      </div>
+    </div>
+
+    <div style={{ marginTop: '15px' }}>
+      <button type='button' className='custom__button'>View More</button>
+    </div>
+  </div >
 );
 
 export default SpecialMenu;
